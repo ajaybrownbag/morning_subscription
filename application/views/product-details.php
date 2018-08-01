c622845c90ed5fa89e3a4bee8afa8cd7b56f977b
SanjayBrownBag <sanjay.sah@brownbag.in>
2018-08-01 14:43:34 +0530

    product details page

--- application/views/product-details.php
+++ application/views/product-details.php
@@ -17,8 +17,17 @@
                         <img src="<?=base_url("assets/img/offers/atta.png");?>" alt="" />
 						<div class="clearfix"></div>
 						<hr class="hr"><br>
-						<h1 class="text-left product-title"><?=$details->product_name;?>  <span class="pull-right"><?=$details->unit;?></span></h1>
-						<p class="text-muted text-left"><b>Brand: </b><?=$details->product_brand;?></p>						
+						<h1 class="text-left product-title"><?=$details->product_name;?></h1>
+
+						<div class="row">
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<p class="text-muted text-left"><b>Brand: </b><?=$details->product_brand;?></p>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-6">
+								<p><span class="pull-right"><b class="text-muted">Quantity:</b>  <?=$details->unit;?></span></p>
+							</div>
+
+						</div>
 						<div class="hidden-sm hidden-xs">
 							<hr class="hr"><br>
 							<p class="text-left"><?=$details->product_description;?></p>
@@ -33,29 +42,168 @@
 								<span class="discount"><i class="fa fa-inr"></i>21.00</span>
 							</span>
                         </div>
-						<span class="btn btn-warning no-radius btn-xs text-muted">2% OFF</span>
+						<span class="btn btn-warning no-radius btn-xs text-muted m-t-15 m-l-10">2% OFF</span>
                         <div class="pull-right">
 							<ul class="product-category">
-								<li>Cut off Time</li>
+								<li><span data-toggle="tooltip" data-placement="top" title="Default tooltip dfgfdggdfgfdg gfdgfd dfgdf dfg">Cut off Time</span></li>
 								<li>-</li>
 								<li><b><?=date("h:iA",strtotime($details->cutoff_time));?></b></li>
 							</ul>
 						</div>
-						<div class="clearfix"></div>
                     </div>
-					<div class="clearfix"></div>
+					<div class="product-warranty">
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label col-md-2 col-sm-2 col-xs-12"><span class="p-t-5 inline-block"><b>Quantity</b></span> <span class="text-danger">*</span></label>
+								<div class="col-md-10 col-sm-10 col-xs-12">
+									<div class="form-inline">
+										<div class="form-group">
+											<select name="dd" class="form-control">
+												<option value="">Select</option>
+												<option value="1" >1 </option>
+												<option value="2" >2 </option>
+												<option value="3" >3 </option>
+												<option value="4" >4 </option>
+												<option value="5" >5 </option>
+												<option value="6" >6 </option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 					<div class="row">
 						<div class="col-sm-12 product-config-container">
 							<ul class="nav nav-tabs">
-							  <li class="active" style="width:50%;text-align:center"><a data-toggle="tab" href="#pattern-container">PATTERN</a></li>
-							  <li style="width:50%;text-align:center"><a data-toggle="tab" href="#once-container">ONCE</a></li>
+							  <li class="active" style="width:50%;text-align:center"><a class="m-0 no-radius" data-toggle="tab" href="#pattern-container">PATTERN</a></li>
+							  <li style="width:50%;text-align:center"><a class="m-0 no-radius" data-toggle="tab" href="#once-container">ONCE</a></li>
 							</ul>
 							<div class="tab-content">
-							  <div id="pattern-container" class="tab-pane fade in active">
-								Hello Testing
-							  </div>
-							  <div id="once-container" class="tab-pane fade">
-								<p>Some content in menu 1.</p>
+							  <div id="pattern-container" class="tab-pane fade in active bg-f5f5f5">
+								<div class="w-100 p-t-10 p-b-10">
+									<div class="form-group m-t-15">
+										<label class="control-label col-md-3 col-sm-3 col-xs-12"><span class="p-t-5 inline-block"><i class="fa fa-calendar"></i> Date Range</span></label>
+										<div class="col-md-9 col-sm-9 col-xs-12">
+											<div class="form-inline">
+												<div class="form-group w-100">
+													<input type="text" class="form-control w-100" name="contactnum" placeholder="Enter your Primary contact no." value="">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="clearfix"></div>
+									<div class="col-md-12 col-sm-12 col-xs-12 m-t-20">
+									<ul class="nav nav-tabs nav-inner-tabs">
+										<li class="active" style="text-align:center"><a class="m-0 no-radius" data-toggle="tab" href="#everydays">Every Days</a></li>
+										<li style="text-align:center"><a class="m-0 no-radius" data-toggle="tab" href="#alternatedays">Alternate Days</a></li>
+										<li style="text-align:center"><a class="m-0 no-radius" data-toggle="tab" href="#weekdays">Week Days</a></li>
+									</ul>
+									<div class="tab-content bg-white">
+									  <div id="everydays" class="tab-pane fade in active bg-white" style="display: none;"></div>
+									  <div id="alternatedays" class="tab-pane fade bg-white">
+											<div class="form-group w-100 m-t-20">
+												<label class="control-label col-md-4 col-sm-4 col-xs-12"><span class="p-t-5 inline-block"><b>Select Alternate Day</b></span></label>
+												<div class="col-md-8 col-sm-8 col-xs-12">
+													<div class="form-inline">
+														<div class="form-group">
+															<select name="dd" class="form-control">
+																<option value="">Select</option>
+																<option value="1" >1 </option>
+																<option value="2" >2 </option>
+																<option value="3" >3 </option>
+																<option value="4" >4 </option>
+																<option value="5" >5 </option>
+																<option value="6" >6 </option>
+															</select>
+														</div>
+													</div>
+												</div>
+											</div>
+									  </div>
+									   <div id="weekdays" class="tab-pane fade bg-white">
+									   		<div class="form-group w-100 m-t-20">
+												<label class="control-label col-md-3 col-sm-3 col-xs-12"><span class="inline-block"><b>Weekdays</b></span></label>
+												<div class="col-md-9 col-sm-9 col-xs-12">
+													<div class="form-inline">
+														<div class="form-group">
+															<ul class="list-inline weekdaytab">
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">MON</button>
+																	<input type="checkbox" class="hidden" />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">TUE</button>
+																	<input type="checkbox" class="hidden" checked />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">WED</button>
+																	<input type="checkbox" class="hidden"  />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">THU</button>
+																	<input type="checkbox" class="hidden" checked />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">FRI</button>
+																	<input type="checkbox" class="hidden"  />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">SAT</button>
+																	<input type="checkbox" class="hidden" checked />
+																	</span>
+															    </li>
+															    <li>
+																	<span class="button-checkbox">
+																	<button type="button" class="btn btn-xs" data-color="blue1">SUN</button>
+																	<input type="checkbox" class="hidden" checked />
+																	</span>
+															    </li>
+															  </ul>
+														</div>
+													</div>
+												</div>
+											</div>
+									  </div>
+									  <div class="w-100">
+										  <div class="col-md-12 col-sm-12 col-xs-12">
+												<div class="alert alert-success alert-dismissible p-t-5 p-b-5 m-t-20">
+												<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+												<strong>Success!</strong> This alert box could indicate a successful or positive action.
+												</div>
+										  </div>
+										  <div class="col-md-12 col-sm-12 col-xs-12 m-b-20 m-t-20">
+												<button class="btn btn-sm btn-blue pull-right">Add to Cart</button>
+										  </div>
+									  </div>
+									</div>
+								</div>
+								</div>
+							</div>
+							  <div id="once-container" class="tab-pane fade bg-f5f5f5">
+							  	<div class="w-100 p-t-10 p-b-10">
+									<div class="form-group m-t-15">
+										<label class="control-label col-md-3 col-sm-3 col-xs-12"><span class="p-t-5 inline-block"><i class="fa fa-calendar"></i> Once</span></label>
+										<div class="col-md-9 col-sm-9 col-xs-12">
+											<div class="form-inline">
+												<div class="form-group w-100">
+													<input type="text" class="form-control w-100" name="contactnum" placeholder="Enter your Primary contact no." value="">
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
 							  </div>
 							</div>
 						</div>
--- assets/js/common.js
+++ assets/js/common.js
@@ -157,4 +157,58 @@ $('.searchwidth').css('min-width','100%');
     }); 
 };
 headeradjustmob();
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
 
+function productdetail(){
+$(function () {
+    $('.button-checkbox').each(function () {
+        var $widget = $(this),
+            $button = $widget.find('button'),
+            $checkbox = $widget.find('input:checkbox'),
+            color = $button.data('color'),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+        $button.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+            $button.data('state', (isChecked) ? "on" : "off");
+            $button.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$button.data('state')].icon);
+            if (isChecked) {
+                $button
+                    .removeClass('btn-disabled')
+                    .addClass('btn-' + color + ' active');
+            }
+            else {
+                $button
+                    .removeClass('btn-' + color + ' active')
+                    .addClass('btn-disabled');
+            }
+        }
+        function init() {
+            updateDisplay();
+            if ($button.find('.state-icon').length == 0) {
+                $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+            }
+        }
+        init();
+    });
+});
+};
+productdetail();
